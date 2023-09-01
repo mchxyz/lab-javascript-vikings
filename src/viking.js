04c05471ada884d637b56f304bc6b74048197e3c
mchxyz <michellearguelles@protonmail.com>
2023-09-01 16:57:20 -0400

    first iteration

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,32 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health 
+        this.strength = strength
+    }
+
+    attack() {
+return this.strength;
+    }
+
+    receiveDamage(damage) {
+     this.health -= damage;
+}
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super (name, health, strength) 
+}
+
+}
+
 
 // Saxon
 class Saxon {}
 
 // War
 class War {}
+
